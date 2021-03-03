250a3ea46785240a30b9da15c9c3d4c1eaaa86f8
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-03 01:44:20 -0600

    Cleaned up formatting.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,32 @@
 // ===========================================================
 // Dependencies
 // ===========================================================
+// Express
 const router = require("express").Router();
-const db = require('../models/');
+// Models
+const db = require("../models/");
 
 // ===========================================================
 // API Routes
 // ===========================================================
     
-// Show last workout
+// Show last workout in the index/home page
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
         .then(dbWorkout => {
             console.log(dbWorkout);
             res.json(dbWorkout);
-        })
-        .catch(error => {
+        }).catch(error => {
             res.status(400).json(error);
         });
 });
 
-// Add a workout
+// Add a workout in the exercise page
 router.post("/api/workouts", ({body}, res) => {
     db.Workout.create(body)
         .then(dbWorkout => {
             res.json(dbWorkout);
-        })
-        .catch(error => {
+        }).catch(error => {
             res.status(400).json(error);
         });
 });
@@ -35,7 +35,7 @@ router.post("/api/workouts", ({body}, res) => {
 router.put("/api/workouts/:id", (req, res) => {
     db.Workout.updateOne(
         { _id: req.params.id },
-        { $push: { workouts: req.body}}
+        { $push: {exercises: req.body}}
     ).then (dbWorkout => {
         res.json(dbWorkout);
     }).catch (error => {
@@ -43,7 +43,7 @@ router.put("/api/workouts/:id", (req, res) => {
     });
 });
 
-// View several workouts
+// View several workouts while in the stats page
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
         .sort({ _id: -1 })
