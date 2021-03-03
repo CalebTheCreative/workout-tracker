189685eee6663cfd944b6131ab8a82cbb55383ce
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-02 23:52:08 -0600

    Updated for db call.

--- models/workout.js
+++ models/workout.js
@@ -7,49 +7,59 @@ const mongoose = require("mongoose");
 // Model Schema
 // ===========================================================
 const Schema = mongoose.Schema;
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    workouts: [
-        {
-            type:
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
             {
-                type: String,
-                trim: true,
-                required: "Enter a workout type"
-            },
-            name:
-            {
-                type: String,
-                trim: true,
-                required: "Enter a workout name"
-            },
-            duration:
-            {
-                type: Number,
-                required: "Enter the duration (min)"
-            },
-            weight:
-            {
-                type: Number
-            },
-            reps:
-            {
-                type: Number
-            },
-            sets:
-            {
-                type: Number
-            },
-            distance:
-            {
-                type: Number
+                type:
+                {
+                    type: String,
+                    trim: true,
+                    required: "Enter a workout type"
+                },
+                name:
+                {
+                    type: String,
+                    trim: true,
+                    required: "Enter a workout name"
+                },
+                duration:
+                {
+                    type: Number,
+                    required: "Enter the duration (min)"
+                },
+                weight:
+                {
+                    type: Number
+                },
+                reps:
+                {
+                    type: Number
+                },
+                sets:
+                {
+                    type: Number
+                },
+                distance:
+                {
+                    type: Number
+                }
             }
-        }
-    ]
-})
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    });
+
+workoutSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((total, exercise) => total + exercise.duration,0);
+});
 
 // ===========================================================
 // Setup for export
