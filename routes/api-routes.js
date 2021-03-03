958750f31143029f5fa9853a0c56e2fbfe669e45
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-02 22:46:16 -0600

    Updated to include range route.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,7 +8,7 @@ const Workout = require("../models/workout.js");
 // API Routes
 // ===========================================================
     
-// Show last workout(s)
+// Show last workout
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
         .then(dbWorkout => {
@@ -28,6 +28,30 @@ router.post("/api/workouts", (req, res) => {
         })
         .catch(error => {
             res.status(400).json(error);
+        });
+});
+
+// Update a workout
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.updateOne(
+        { _id: req.params.id },
+        { $push: { workouts: req.body}}
+    ).then (dbWorkout => {
+        res.json(dbWorkout);
+    }).catch (error => {
+        res.json(error);
+    });
+});
+
+// View several workouts
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .sort({ _id: -1 })
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        }).catch(error => {
+            res.json(error);
         });
 });
 
