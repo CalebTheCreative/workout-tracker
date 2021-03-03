a3273814cf1121c3bb47dc9c195aa1c30b8ffc48
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-02 22:50:17 -0600

    Added HTML routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,26 @@
+// ===========================================================
+// Dependencies
+// ===========================================================
+const path = require("path");
+const router = require('express').Router();
+
+// ===========================================================
 // HTML Routes
+// ===========================================================
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+// ===========================================================
+// Export
+// ===========================================================
+module.exports = router;
