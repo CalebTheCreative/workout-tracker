abf082ed40ba0c72e4a0848b6462f4b3644128df
Caleb Hopkins <CalebHopkins@outlook.com>
2021-03-02 22:27:15 -0600

    Added 'get' route.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,6 +7,19 @@ const Workout = require("../models/workout.js");
 // ===========================================================
 // API Routes
 // ===========================================================
+    
+// Show last workout(s)
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
 
 // ===========================================================
 // Export
